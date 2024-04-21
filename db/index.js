9c3eaddc604a53cfff4b107261e372d48aa6df7e
DevPBhardwaj <bhardwajpriyanshu300@gmail.com>
2024-04-21 09:11:35 -0400

    Connect to Mongo Atlas

---
+++ db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/Constants.js";
+
+
+const connectDB = async () =>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB Connected Successfully ! DB HOST : ${connectionInstance.connection.host}`);
+    }
+    catch(error){
+        console.log("MongoDB Connection Error:",error);
+        process.exit(1)    //this is the node method you can check more in node documentations about the exits number like 1,0 etc
+    }
+}
+
+
+export default connectDB;
